c05427ae0d4836b1ebf10d8afa2af1fade348927
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-07-29 21:40:06 +0100

    improved semantic versioning order for latest version

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Samsara Leiningen Template
-The Samsara template has everything you need to get a processing pipeline up and running quickly. The template provides you with a starter processing core, a main, a test, and docker files to deploy your project locally.
+
+The Samsara template has everything you need to get a processing
+pipeline up and running quickly. The template provides you with a
+starter processing core, a main, a test, and docker files to deploy
+your project locally.
 
 ## Usage
-To create a new Samsara project, run `lein new samsara <project-name>`. By default, the new project will use the latest release version of Samsara. If you wish to use a specific version, you can se the version by adding the `--with-version <version-number>` argument. To use the latest snapshot, add the `--with-snapshot` argument. 
+
+To create a new Samsara project, run:
+
+    lein new samsara <project-name> --with-version <version-number>]
+
+By default, the new project will use the latest stable release version
+of Samsara. If you wish to use a specific version, you can set the
+version by running:
+
+    lein new samsara <project-name> --with-version <version-number>
+
+To use the latest release including snapshot, add the `--with-snapshot` argument:
+
+    lein new samsara <project-name> --with-snapshot
+
+## License
+
+Copyright © 2015 Samsara's authors.
+
+Distributed under the Apache License v 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
--- src/leiningen/new/samsara.clj
+++ src/leiningen/new/samsara.clj
@@ -1,7 +1,8 @@
 (ns leiningen.new.samsara
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :as main]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [clojure.string :as str]))
 
 (def ^:const SUPPORTED-OPTIONS ["--with-snapshot" "--with-version"])
 
@@ -19,16 +20,47 @@
               (not (some #(= (:version %) version) versions))))
     {:error "Invalid Samsara version."}))
 
-(defn get-latest-version
-  [key]
-  (key (read-string (:body (http/get CLOJARS_URL {:accept :edn})))))
+
+(defn sort-by-semantic-version [versions]
+  (->> versions
+       (map (partial re-find #"(\d+)\.(\d+)\.(\d+)(?:\.(\d+))?(?:-(SNAPSHOT))?"))
+       (map (fn [[v a b c d sn]]
+              (let [->int (fnil read-string "-1")]
+                [(->int a) (->int b) (->int c) (->int d) (not= sn "SNAPSHOT") v])))
+       sort
+       (map last)))
+
+
+(defn samsara-versions
+  []
+  (main/info "Searching for latest Samsara's version on Clojars.org ...")
+  (->> (read-string (:body (http/get CLOJARS_URL {:accept :edn})))
+       :recent_versions
+       (map :version)))
+
+
+(defn latest-version
+  [versions snapshot?]
+  (->> versions
+       sort-by-semantic-version
+       (filter #(if snapshot? true (not (re-find #"SNAPSHOT" %))))
+       last))
+
+
+(defn samsara-latest-version [snapshot?]
+  (let [latest (latest-version (samsara-versions) snapshot?)]
+    (main/info "Samsara latest version:" latest)
+    latest))
+
 
 (defn parse-version
   [args options]
   (cond
     (contains? args :--with-version) (assoc options :version (:--with-version args) :docker-version (:--with-version args))
-    (contains? args :--with-snapshot) (assoc options :version (get-latest-version :latest_version) :docker-version "snapshot")
-    :else (let [latest-release (get-latest-version :latest_release)] (assoc options :version latest-release :docker-version latest-release))))
+    (contains? args :--with-snapshot) (assoc options :version (samsara-latest-version true) :docker-version "snapshot")
+    :else (let [latest-release (samsara-latest-version false)]
+            (assoc options :version latest-release :docker-version latest-release))))
+
 
 (defn validate-and-parse-arguments
   "Verifies that all template specific arguments supplied to the lein new command are valid options. Also checks to see
@@ -40,6 +72,20 @@
           (main/info "Both --with-version and --with-snapshot arguments were found. Samsara will use the --with-version value."))
         (parse-version args {}))))
 
+
+(defn headline []
+  (main/info
+   "
+-----------------------------------------------
+   _____
+  / ___/____ _____ ___  _________ __________ _
+  \\__ \\/ __ `/ __ `__ \\/ ___/ __ `/ ___/ __ `/
+ ___/ / /_/ / / / / / (__  ) /_/ / /  / /_/ /
+/____/\\__,_/_/ /_/ /_/____/\\__,_/_/   \\__,_/
+
+-----------------------------------------------
+"))
+
 (defn samsara
   "A leinengen template to get you started with Samsara. Sets up a simple processor pipeline, required config files, and
   a docker-compose file to get you up and running quickly."
@@ -50,7 +96,8 @@
                   :sanitized      (name-to-path name)
                   :version        (:version parsed)
                   :docker-version (:docker-version parsed)}]
-        (main/info "Generating fresh 'lein new' samsara project.")
+        (headline)
+        (main/info "Generating fresh Samsara project.")
         (->files data
                  ["project.clj" (render "project.clj" data)]
                  ["src/{{sanitized}}/main.clj" (render "main.clj" data)]
@@ -62,6 +109,10 @@
                  ["LICENSE" (render "LICENSE")]
                  ["doc/intro.md" (render "intro.md")]
                  [".gitignore" (render ".gitignore")]
-                 [".hgignore" (render ".hgignore")]
-                 "resources"))
+                 #_[".hgignore" (render ".hgignore")] ;; missing
+                 "resources")
+        (main/info "All done.\n")
+        (main/info "To build and test:\n\t$ lein do clean, midje, bin")
+        (main/info "To run:\n\t$ docker-compose up -d\n\t$ lein run -- config/config.edn\n")
+        )
       (main/info (:error parsed)))))
